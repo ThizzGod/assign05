69e8fe8d32e30d554493ad5305718cee566403c6
jgac72 <jgac72@gmail.com>
2025-09-27 16:05:20 -0600

    Add files via upload

--- FirstPivotChooser.java
+++ FirstPivotChooser.java
@@ -2,12 +2,11 @@ package assign05;
 
 import java.util.List;
 
-public class FirstPivotChooser<E extends Comparable<? super E>> implements PivotChooser<E> {
+public class FirstPivotChooser<E extends Comparable<? super E>> implements PivotChooser<E>{
 
 	@Override
 	public int getPivotIndex(List<E> list, int leftIndex, int rightIndex) {
-		// TODO Auto-generated method stub
-		return 0;
+		return leftIndex;
 	}
 
 }
